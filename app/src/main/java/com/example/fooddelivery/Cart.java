47af925b6731c421e9f85bc571046ee80ae1739a
PopCristinelMihai <battlefieldheroes43@gmail.com>
2019-02-18 04:17:43 +0200

    -Added price,discount,productId,productName and quantity values -Added -Added placing order button functionality -Added place order address -Added firebase requests ~TODO~ -Add order info tracking        -Add sign out capability

--- app/src/main/java/com/example/fooddelivery/Cart.java
+++ app/src/main/java/com/example/fooddelivery/Cart.java
@@ -1,13 +1,21 @@
 package com.example.fooddelivery;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.fooddelivery.Common.Common;
 import com.example.fooddelivery.Database.Database;
 import com.example.fooddelivery.User.Order;
+import com.example.fooddelivery.User.Request;
 import com.example.fooddelivery.ViewHolder.CartAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,7 +32,7 @@ public class Cart extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     FirebaseDatabase database;
-    DatabaseReference request;
+    DatabaseReference requests;
     TextView txtTotalPrice;
     FButton btnPlace;
 
@@ -39,7 +47,7 @@ public class Cart extends AppCompatActivity {
 
         //Firebase
         database=FirebaseDatabase.getInstance();
-        request=database.getReference("Requests");
+        requests=database.getReference("Requests");
 
 
 
@@ -52,8 +60,66 @@ public class Cart extends AppCompatActivity {
         txtTotalPrice=(TextView)findViewById(R.id.total);
         btnPlace=(FButton)findViewById(R.id.btnPlaceOrder);
 
+        btnPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showAlertDialog();
+            };
+        });
+
         loadListFood();
 
+    }
+
+
+    private void showAlertDialog() {
+
+        AlertDialog.Builder alertDialog=new AlertDialog.Builder(Cart.this);
+        alertDialog.setTitle("One more step");
+        alertDialog.setMessage("Enter your adress");
+
+        final EditText edtAdress=new EditText(Cart.this);
+        LinearLayout.LayoutParams lp=new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        edtAdress.setLayoutParams(lp);
+        alertDialog.setView(edtAdress); //Add edit text to alert dialog
+        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+        alertDialog.setPositiveButton("Place Order", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Create new request
+                Request request=new Request(
+                        Common.currentUser.getPhone(),
+                        Common.currentUser.getName(),
+                        edtAdress.getText().toString(),
+                        txtTotalPrice.getText().toString(),
+                        cart
+                );
+
+                //Submit to firebase
+                //Using System.CurentMill to key
+                requests.child(String.valueOf(System.currentTimeMillis()))
+                        .setValue(request);
+                //Delete cart
+
+                new Database(getBaseContext()).cleanCart();
+                Toast.makeText(Cart.this,"Your order has been placed!",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
     }
 
     private void loadListFood() {
